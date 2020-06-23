b3cba322664ffb474593b691f03c436807a5e7a7
= <jsw2095@naver.com>
2020-06-24 03:08:22 +0900

    Alias Component Styling

--- src/components/Alias.tsx
+++ src/components/Alias.tsx
@@ -5,23 +5,38 @@ type AliasProps = {
   name: string;
 };
 
-const AliasBox = styled.div`
+const AliasBox = styled.button`
   text-align: center;
   justify-content: center;
-  font-size: 2rem;
-  padding: 1rem;
+  font-size: 1.3rem;
+  padding: 0.8rem;
   background: #12b886;
-  border: 1px solid black;
+  border: none;
   border-radius: 5px;
-  width: 40rem,
   cursor: pointer;
+
+  &:hover {
+    background: #20c997;
+  }
+  &:active {
+    background: #0ca678;
+  }
+
+  span {
+    color: #c92a2a;
+    font-weight: bold;  
+  }
+`;
+
+const AliasVariable = styled.span`
+  color: #c92a2a;
+  font-weight: bold;
 `;
 
 function Alias({ name }: AliasProps) {
   return (
     <AliasBox>
-      <span>tained</span>
-      <span>= _.{name}()</span>
+      <span>tained</span> = _.{name}()
     </AliasBox>
   )
 }
