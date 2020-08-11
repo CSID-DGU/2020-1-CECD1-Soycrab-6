c484ceb3d32d14179a3a22ac6534b9404c7c3a4a
YJHoon <yjhoon0816@gmail.com>
2020-08-11 17:17:01 +0900

    edit page base

--- src/pages/NodeEditPage.js
+++ src/pages/NodeEditPage.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Container } from 'react-bootstrap'
 import NodeEditContainer from '../containers/NodeEditContainer';
+import HeadNavbar from '../components/HeadNavbar';
 
 function NodeEditPage({ match }) {
   const { id } = match.params;
   const nodeId = parseInt(id, 10);
 
-  return <NodeEditContainer nodeId={nodeId} />;
+  const Container  = styled.div`
+
+  `
+  const VariableBox = styled.div`
+    padding: 1.8rem;
+    text-align: center;
+    border: 1px solid #73ca25
+  `
+  return (
+    <>
+      <HeadNavbar />
+      <VariableBox> 
+        <NodeEditContainer nodeId={nodeId} />;
+      </VariableBox>
+    </>
+  )
 };
 
 export default NodeEditPage;
