b84672dc5e3201a6fae3681fb0c35013a4113d07
YJHoon <yjhoon0816@gmail.com>
2020-08-11 14:24:27 +0900

    react-popup test

--- src/components/Node.js
+++ src/components/Node.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import Popup from "reactjs-popup";
+import Select from 'react-select';
 import { MdDelete } from 'react-icons/md';
 import { IoIosAddCircleOutline } from 'react-icons/io';
 
@@ -61,11 +63,37 @@ const Remove = styled.div`
   }
 `;
 
+const options = [
+  { value: 'chocolate', label: '호출식' },
+  { value: 'strawberry', label: '접근식' },
+  { value: 'vanilla', label: '연산식' },
+  { value: 'vanilla', label: '배열 참조' },
+  { value: 'vanilla', label: '배열 생성' },
+  { value: 'vanilla', label: '개별 이벤트 템플릿' },
+]
+
+const PopupExample = () => (
+    <Popup trigger={<div><IoIosAddCircleOutline /></div>} position="top left">
+      {close => (
+        <div>
+          <label>추적변수 추가</label>
+          <input type="text" placeholder="start typing ... " />
+          <label>이벤트 추가</label>
+          <Select options={options} />
+          <a className="close" onClick={close}>
+            &times;
+          </a>
+        </div>
+      )}
+    </Popup>
+
+)
+
 function Node({ node }) {
   return (
     <NodeBox>
       <Add>
-        <IoIosAddCircleOutline />
+        <PopupExample />
       </Add>
       <Remove>
         <MdDelete />
