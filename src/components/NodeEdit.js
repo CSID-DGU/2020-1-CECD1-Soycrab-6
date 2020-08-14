8f121dae017e301f94402c370e7c9a1d4f335c4e
YJHoon <yjhoon0816@gmail.com>
2020-08-14 10:44:17 +0900

    스타일 수정

--- src/components/NodeEdit.js
+++ src/components/NodeEdit.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 function NodeEdit({ node }) {
   const { realId, filterId, isEnd, events, traceVars } = node;
 
--- src/pages/NodeEditPage.js
+++ src/pages/NodeEditPage.js
@@ -3,25 +3,55 @@ import styled from 'styled-components';
 import { Container } from 'react-bootstrap'
 import NodeEditContainer from '../containers/NodeEditContainer';
 import HeadNavbar from '../components/HeadNavbar';
+import {Row, Col} from 'reactstrap';
 
 function NodeEditPage({ match }) {
   const { id } = match.params;
   const nodeId = parseInt(id, 10);
 
   const Container  = styled.div`
-
+    padding: 1.8rem;
+    text-align: center;
+    border: 1px solid #73ca25;
+    width: 80%;
+    margin-left:10%;
+    margin-right: 10%;
+    margin-top: 200px;
+    height: auto;
+  `
+  const Header = styled.div`
+    padding: 0.3em;
+    padding-left: 0.6em;
+    width: 100%;
+    text-align: left;
+    background-color: #73ca25;
+    color: white;
+    font-size: 2em;
+    margin-bottom: 30px;
   `
   const VariableBox = styled.div`
     padding: 1.8rem;
     text-align: center;
-    border: 1px solid #73ca25
+    border: 1px solid #73ca25;
+    height: 1em;
+    width: 28vw;
   `
   return (
     <>
       <HeadNavbar />
-      <VariableBox> 
+      <Container>
+        <Header>
+          Alias
+        </Header>
         <NodeEditContainer nodeId={nodeId} />;
-      </VariableBox>
+        <Row>
+          <Col>
+            <VariableBox> 
+              x=aasdsa
+            </VariableBox>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 };
