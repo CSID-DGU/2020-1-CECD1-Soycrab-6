2cbace4f4aed78e5e2ce42ce8e3beafe933c8e1f
Juyeong Lee <2jy22@naver.com>
2020-08-14 16:34:30 +0900

    노드 이벤트 추가 코드 수정

--- src/components/NodeEdit.js
+++ src/components/NodeEdit.js
@@ -55,7 +55,7 @@ function NodeEdit({ node }) {
             </CardBody>
             <ListGroup flush>
               {eventArray.map(
-                (event, index) => <ListGroupItem key={index}><TasksMedia type={event} /></ListGroupItem>
+                (event, index) => <ListGroupItem key={index} action><TasksMedia type={event} /></ListGroupItem>
               )}
             </ListGroup>
           </Card>
