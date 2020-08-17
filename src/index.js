0eaa81a926f770515e569feb5ceb2d935f271468
YJHoon <yjhoon0816@gmail.com>
2020-08-17 15:44:17 +0900

    .babelrc 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
-import 'core-js/features/string/repeat' 
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import 'core-js/features/string/repeat';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
