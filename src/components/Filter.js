5a68c8d9d37965cdea824fad836425af5cc38060
Juyeong Lee <2jy22@naver.com>
2020-08-18 17:51:39 +0900

    useHistory

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 function Filter({ filter }) {
   const { realId, edgeId } = filter;
+  const history = useHistory();
+
+  const goToFilterEdit = function(edgeId, realId) {
+    history.push(`/filters/${edgeId}/${realId}`);
+  };
 
   return (
-    <a href={`/filters/${edgeId}/${realId}`} 
+    <button onClick={() => goToFilterEdit(edgeId, realId)} 
        className="list-group-item list-group-item-action">
          활성필터
-    </a>
+    </button>
   );
 };
 
--- src/components/Propagator.js
+++ src/components/Propagator.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 function Propagator({ propagator }) {
   const { edgeId, realId } = propagator;
+  const history = useHistory();
+
+  const goToPropagatorEdit = function(edgeId, realId){
+    history.push(`/propagators/edit/${edgeId}/${realId}`);
+  }
 
   return (
-    <a href={`/propagators/edit/${edgeId}/${realId}`} className="list-group-item list-group-item-action" key={propagator.realId}>
+    <button onClick={() => goToPropagatorEdit(edgeId, realId)} className="list-group-item list-group-item-action" key={propagator.realId}>
       전파자 #{realId}-#{edgeId}
-    </a>
+    </button>
   );
 };
 
