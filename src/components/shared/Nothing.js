245c4e9b2f0e3f81d3a1d9d7d420343bafdf456b
Juyeong Lee <2jy22@naver.com>
2020-08-21 09:14:46 +0900

    NothingBox cursor, hover 효과 제거

--- src/components/shared/Nothing.js
+++ src/components/shared/Nothing.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const NothingBox = styled.div`
+  cursor: auto;
+
+  &:hover {
+    background-color: inherit;
+  }
+`;
 
 function Nothing() {
-  return <button className="list-group-item list-group-item-action">없음</button>;
+  return <NothingBox className="list-group-item list-group-item-action">없음</NothingBox>;
 }
 
 export default Nothing;
